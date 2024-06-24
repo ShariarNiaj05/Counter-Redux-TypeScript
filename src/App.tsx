43ba4220c13e025c4a7729481b233aab0ca03e7a
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-25 00:09:51 +0600

    base counter ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 function App() {
   return (
-    <>
-      <h1>React App With Redux & TypeScript</h1>
-    </>
+    <div className=" h-screen w-full flex items-center justify-center">
+      <div className=" flex gap-10 border border-purple-300 rounded-md bg-slate-200 p-10">
+        <button className=" px-3 py-2 rounded-md bg-green-400 text-xl font-semibold text-white">
+          Increment
+        </button>
+        <h1 className=" text-3xl my-5">0</h1>
+        <button className=" px-3 py-2 rounded-md bg-red-400 text-xl font-semibold text-white">
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
